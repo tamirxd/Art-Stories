17272afe8321d9527ec645f0391239e7c181f7f7
Tamir Michaeli <michaelitamir@gmail.com>
2020-08-27 20:22:30 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector(".menu-btn"); // Select item
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const barrelImage = document.querySelector(".project-img-1");
+
+// Set Menu initial state
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+function toggleMenu() {
+  if (!showMenu) {
+    // Show menu, hide menu button
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach((navItem) => navItem.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    // Hide menu, show menu button
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach((navItem) => navItem.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
